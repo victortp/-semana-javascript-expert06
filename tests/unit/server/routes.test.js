67cf11c327012cffbf5a73c6e3565683d7c786e7
Victor Teixeira <teixeirapinheiroster@gmail.com>
2022-03-17 14:37:24 -0300

    test: GET / - should redirect to home page

--- tests/unit/server/routes.test.js
+++ tests/unit/server/routes.test.js
@@ -1,7 +1,9 @@
 import { jest, expect, describe, test, beforeEach } from '@jest/globals';
 import config from '../../../server/config.js';
+import TestUtil from '../_util/testUtil.js';
+import { handler } from '../../../server/routes.js';
 
-const { pages } = config;
+const { pages, location } = config;
 
 describe('#Routes - test suite for api response', () => {
   beforeEach(() => {
@@ -9,7 +11,18 @@ describe('#Routes - test suite for api response', () => {
     jest.clearAllMocks();
   });
 
-  test.todo('GET / - should redirect to home page');
+  test('GET / - should redirect to home page', async () => {
+    const params = TestUtil.defaultHandleParams();
+    params.request.method = 'GET';
+    params.request.url = '/';
+
+    await handler(...params.values());
+
+    expect(params.response.writeHead).toHaveBeenCalledWith(302, {
+      Location: location.home
+    });
+    expect(params.response.end).toHaveBeenCalled();
+  });
   test.todo(`GET /home - should respond with ${pages.homeHTML} file stream`);
   test.todo(`GET /controller - should respond with ${pages.controllerHTML} file stream`);
   test.todo(`GET /file.txt - should respond with file stream`);
